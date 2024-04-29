bdd91003dde2d3c7db68b98b937ae0431e280af3
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-04-29 10:21:19 +0100

    tasks 0-3

---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from . import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """Return a JSON indicating the status of the API."""
+    return jsonify({"status": "OK"})
