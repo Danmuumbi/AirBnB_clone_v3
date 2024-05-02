0af47b7bfe98d10afd0b13ffabb30aee61b07828
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-05-02 23:21:11 +0100

    task4

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,15 +5,16 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
-@app.route('/')
-def hello():
-    """
-    Hello world
-    """
-    return 'Hello, World!'
+# @app.route('/')
+# def hello():
+#     """
+#     Hello world
+#     """
+#     return 'Hello, World!'
 
 
 @app.errorhandler(404)
@@ -34,4 +35,4 @@ if __name__ == "__main__":
     import os
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
