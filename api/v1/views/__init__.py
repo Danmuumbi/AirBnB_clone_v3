977388ed63b921b9fe7ab08145530f9ec42ac798
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-05-02 07:34:25 +0100

    pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,25 +1,15 @@
-# #!/usr/bin/python3
-# """Define modules"""
-# from flask import Blueprint
-
-# """Add blueprint"""
-# app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# from api.v1.views.index import *
-# from api.v1.views.states import *
-
 #!/usr/bin/python3
-"""create blueprint"""
+"""Define modules"""
 from flask import Blueprint
 
+"""Add blueprint"""
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-if app_views is not None:
-    from api.v1.views.index import *
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    # from api.v1.views.amenities import *
-    # from api.v1.views.users import *
-    # from api.v1.views.places import *
-    # from api.v1.views.places_reviews import *
-    # from api.v1.views.places_amenities import *
+
+def configure_views(app):
+    from .index import status, stats
+    from .states import (
+        get_states, get_state, delete_state,
+        create_state, update_state
+        )
+    app.register_blueprint(app_views)
